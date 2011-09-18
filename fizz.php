c97fc510a0723aa62f616a70e7d5fad027d04e7e
Rick <rick.matthews@gmail.com>
2011-09-18 14:16:28 -0400

    comments added

---
+++ fizz.php
@@ -0,0 +1,16 @@
+<?php
+
+//initialize variable
+$i=0;
+
+for ($i=1; $i<101; $i++)
+{
+	if ($i % 3 == 0)
+	{
+		if ($i % 6 == 0) { echo "fizz-buzz <br />\n"; }
+		else { echo "fizz <br />\n"; }
+	}
+	else { echo $i . "<br />\n"; }
+}
+
+?>
